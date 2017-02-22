e4a8ffd82f99b3928a5536a3114c76e1ae452426
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-21 16:17:00 -0800

    Comment out unused code, but retain for reference

--- lib/markdown/0-load.php
+++ lib/markdown/0-load.php
@@ -1,4 +1,5 @@
 <?php
-if ( ! class_exists( 'MarkdownExtra_Parser' ) )
-	jetpack_require_lib( 'markdown/extra' );
-jetpack_require_lib( 'markdown/gfm' );
+
+//if ( ! class_exists( 'MarkdownExtra_Parser' ) )
+//	jetpack_require_lib( 'markdown/extra' );
+//jetpack_require_lib( 'markdown/gfm' );
